173c8a6386b5d2f67c8b80427c3eebea924f7151
Merlin Mason <info@merlinmason.co.uk>
2013-12-16 16:46:39 +0000

    fix library layout

--- page.php
+++ page.php
@@ -11,24 +11,21 @@
             <h2><?php the_field('showreel_title', 'options'); ?></h2>
         </article>
 
-        <section class="row">
-            <?php
-                $args = array( 'numberposts' => 99, 'child_of' => '6' );
-                $lastposts = get_posts( $args );
-                foreach($lastposts as $post) : setup_postdata($post);
-            ?>
+        <section class="row"><?php
+            $args = array( 'numberposts' => 99, 'child_of' => '6' );
+            $lastposts = get_posts( $args );
+            foreach($lastposts as $post) : setup_postdata($post);
+            // funky formatting below to ensure no whitespace between articles
+            // which may break inline-block layout
+            ?><article class="col-sm-6 post">
 
-                <article class="col-sm-6 post">
-                    <div class="post-contents">
-                        <img src="<?php the_field('thumb'); ?>">
-                        <i class="play-video" data-video="<?php the_field('vimeo_id'); ?>"></i>
-                    </div>
-                    <h2><?php the_title(); ?> - <?php the_field('desc_short'); ?></h2>
-                </article>
+                <div class="post-contents">
+                    <img src="<?php the_field('thumb'); ?>">
+                    <i class="play-video" data-video="<?php the_field('vimeo_id'); ?>"></i>
+                </div>
+                <h2><?php the_title(); ?> - <?php the_field('desc_short'); ?></h2>
 
-            <?php endforeach; ?>
-            <?php wp_reset_query(); ?>
-        </section>
+        </article><?php endforeach; ?><?php wp_reset_query(); ?></section>
     </div>
 
     <a href="<?php echo home_url('/'); ?>" class="back-home">Back to the homepage</a>
