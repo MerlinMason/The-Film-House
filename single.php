ad6bfb20a5cc296ba406ad26767a863cff25c23c
Merlin Mason <info@merlinmason.co.uk>
2013-12-17 15:11:21 +0000

    check there's actually an image uploaded

--- single.php
+++ single.php
@@ -3,9 +3,13 @@
         <?php if(get_field('slides')) { ?>
             <ul class="slides">
                 <?php while(has_sub_field('slides')) { ?>
-                    <li>
-                        <img src="<?php the_sub_field('slide'); ?>" alt="<?php the_title(); ?>">
-                    </li>
+                    <?php if(get_sub_field('slide')) {
+                        // check there's actually an image uploaded to the instance
+                    ?>
+                        <li>
+                            <img src="<?php the_sub_field('slide'); ?>" alt="<?php the_title(); ?>">
+                        </li>
+                    <?php } ?>
                 <?php } ?>
             </ul>
         <?php } ?>
