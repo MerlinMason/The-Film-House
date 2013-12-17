bc4d3aae2fb2e9301eda314ac02946b44b444b7c
Merlin Mason <info@merlinmason.co.uk>
2013-12-17 20:31:31 +0000

    switch to 8 items in the homepage menu

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
             href="#1" class="current"><span class="title" data-title="0"></span></a><a
             href="#2"><span class="title" data-title="1"></span></a><a
             href="#3"><span class="title" data-title="2"></span></a><a
-            href="#4"><span class="title" data-title="3"></span></a><a
-            href="#5"><span class="title" data-title="4"></span>
+            href="#4"><span class="title" data-title="3"></span>
         </a></nav>
 
         <nav class="slides-pagination secondary"><a
-            href="#5"><span class="title" data-title="5"></span></a><a
-            href="#6"><span class="title" data-title="6"></span></a><a
-            href="#7"><span class="title" data-title="7"></span></a><a
-            href="#8"><span class="title" data-title="8"></span></a><a
-            href="#9"><span class="title" data-title="9"></span>
+            href="#5"><span class="title" data-title="4"></span></a><a
+            href="#6"><span class="title" data-title="5"></span></a><a
+            href="#7"><span class="title" data-title="6"></span></a><a
+            href="#8"><span class="title" data-title="7"></span>
         </a></nav>
 
         <nav class="slides-navigation">
@@ -25,7 +23,7 @@
         <ul class="slides-container">
 
             <?php
-                $args = array('numberposts' => 10, 'category' => '4');
+                $args = array('numberposts' => 8, 'category' => '4');
                 $lastposts = get_posts( $args );
                 foreach($lastposts as $post) : setup_postdata($post);
             ?>
